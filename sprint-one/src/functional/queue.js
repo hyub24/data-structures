299dc08ced743e9f812ef5e6ea84e64118e9a034
Hack Reactor Student <student@hackreactor.com>
2018-09-19 12:06:14 -0700

    a little better than stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
+  var enqueueNum = 0;
+  var firstInLine = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count++;
+    storage[enqueueNum] = value;
+    enqueueNum++;
   };
 
   someInstance.dequeue = function() {
+    var dequeueVal = storage[firstInLine];
+    if (count !== 0) {
+      count--;
+    }
+    firstInLine++;
+    return dequeueVal;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
