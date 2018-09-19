341f530049eced79ab78f731686e4c5b7d324804
Hack Reactor Student <student@hackreactor.com>
2018-09-19 16:01:21 -0700

    DONE!

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.front = 0;
+    this.back = 0;
+    this.storage = {};
+  }
+
+  enqueue(val) {
+    this.count++;
+    this.storage[this.back] = val;
+    this.back++;
+  }
+
+  dequeue() {
+    if (this.count !== 0) {
+      this.count--;
+    }
+    var dequeueVal = this.storage[this.front];
+    this.front++;
+    return dequeueVal;
+  }
+
+  size() {
+    return this.count;
   }
 
 }
+
+new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.storage = {};
+  }
+
+  push(val) {
+    this.count++;
+    this.storage[this.count] = val;
+  }
+
+  pop() {
+    var popVal = this.storage[this.count];
+    if (this.count !== 0) {
+      this.count--;
+    }
+    return popVal;
+  }
+
+  size() {
+    return this.count;
   }
 
 }
+
+new Stack();
