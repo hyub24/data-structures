ffc9f0230361d726c39ea2aceef3999ad15a3d8b
Hack Reactor Student <student@hackreactor.com>
2018-09-19 11:55:41 -0700

    first one done

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,34 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
+  var num = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    
+    count++;
+    num++;
+    storage[num] = value;
   };
 
   someInstance.pop = function() {
+    
+    if (count !== 0) {
+      count--;
+    }
+    var popValue = storage[num];
+    
+    //storage.num = undefined;
+    if (num !== 0) {
+      num--;
+    }
+
+    return popValue;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
